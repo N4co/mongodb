972b89df2cdb63af3938680ef165c69d7edc69dd
n4co <nf.oliveira@hotmail.com>
2021-10-24 01:39:24 -0300

    Criando Post com usuario aninhado

---
+++ src/main/java/com/nicolasfelix/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.nicolasfelix.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nicolasfelix.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+	
+	
+	
+	
+}
